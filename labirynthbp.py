ad196e46aafcf7ee179832550fecde88262a04b1
gogugogiugogu <o.piotrowski-08@o2.pl>
2025-08-11 16:49:49 +0100

    labyrinth.py

--- labirynthbp.py
+++ labirynthbp.py
@@ -1,18 +1,36 @@
-
 map = {
     "start": {
-        "description": "You are in a dark corridor. You can go north or east. [NSWE]",
+        "description": "You are in a dark corridor. You can go north or east. [NWSE]",
         "N": "hall",
         "E": "tunnel"
     },
     "hall": {
-        "description": "A large echoing hall. You can go back south.",
+        "description": "A large echoing hall. You can go back south, east or north.",
+        "E": "ltunnel",
+        "N": "room",
         "S": "start"
     },
+    "room":{
+         "description": "A small, empty room, what a waste of time, go back south",
+         "S": "hall"
+    },
+    "ltunnel":{
+        "description": "A long uncanny tunnel with a bright light at the end, you can go back west or continue east",
+        "E": "Exit",
+        "W": "hall"
+    },
+    "Exit":{
+        "description": "Congratulations! You found the exit! You can go back west to explore the maze, but dont get lost!",
+        "W": "ltunnel"
+    },
     "tunnel": {
-        "description": "A narrow tunnel leading nowhere. You can go back west.",
-        "W": "start"
-    }
+        "description": "A narrow tunnel leading to a strange door. You can go back west or continue east.",
+        "W": "start",
+        "E": "snake"
+    },
+    "snake": {
+         "description": " A room filled with venomous snakes, before you could react they lunge at you and kill you.",
+},
 }
 
 position = "start"
